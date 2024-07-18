dc12440b326a1cda4ea799e427fc9a2fa6c535dc
Paul <paulopost20@gmail.com>
2024-07-18 11:31:04 +0200

    Add ATR

--- controlstation/Launchpad.py
+++ controlstation/Launchpad.py
@@ -20,14 +20,23 @@ from Devices import Printer, EDM
 
 class MouseControlApp:
     def __init__(self, root):
+        self.data_y = [1024,988.25,962.25,941.75,926.75,915.75,908,899.75,894,888.5,888.5,884,880.5,877.75,873.75,871,869.25,
+             866.5,865,863,861.25,859.75,859,857.5,855.5,855.25,853.25,852.5,851.25,850.75,849,848.5,848,847.5,830.5]
+        
+        self.data_dist = [0.9643,1.1884,1.4208,1.6667,1.918,2.1509,2.3877,2.6337,2.8796,3.1149,3.115,3.3526,3.5916,3.8396,4.0767,4.3259,4.5668,
+             4.8044,5.0484,5.2957,5.5318,5.7694,6.0164,6.2626,6.5009,6.7531,7.2202,7.4646,7.7897,8.0302,8.4383,8.6779,8.9205,9.1663,20.7122]
+        
+        self.data_x = [1026,1019.25,1016.5,1015.25,1013.75,1013.5,1012,1011.25,1010.75,1010.5,1010.75,1010.75,
+                        1009,1009.5,1009.5,1009.25,1009,1008.5,1008.75,1008.75,1008.5,1008,1007.5,1007.5,1007.5,1007.75,1008,1007,1007,1007.25,1006.75,1006.75,1007,1006.5,1003]
+
         self.root = root
         self.root.title("OpenTachy - Control")
         
         # Screen dimensions
         self.WIDTH, self.HEIGHT = 1024, 750
         
-        self.printer = Printer("/dev/ttyPRI", 250000)
-        self.edm = EDM("/dev/ttyEDM", 19200)
+        self.printer = Printer("/dev/usbPRI", 250000)
+        self.edm = EDM("/dev/usbEDM", 19200)
         # Create main frame
         self.main_frame = tk.Frame(self.root)
         self.main_frame.pack(fill=tk.BOTH, expand=True)
@@ -62,6 +71,9 @@ class MouseControlApp:
         self.start_edm_button = tk.Button(self.sidebar, text="Connect EDM", command=self.start_edm)
         self.start_edm_button.pack(pady=5)
 
+        self.change_laser = tk.Button(self.sidebar, text="Laser: OFF", command=self.switch_laser)
+        self.change_laser.pack(pady=5)
+
         self.get_distance_button = tk.Button(self.sidebar, text="Get Distance", command=self.get_distance)
         self.get_distance_button.pack(pady=5)
 
@@ -72,6 +84,7 @@ class MouseControlApp:
         self.change_atr.pack(pady=5)
 
         self.position = True
+        self.laser_state = False
         self.atr_state = False 
         #self.laser_detection_button = tk.Button(self.sidebar, text="ATR: OFF", command=self.toggle_laser_detection)
         #self.laser_detection_button.pack(pady=5)
@@ -111,13 +124,13 @@ class MouseControlApp:
 
     def start_printer(self):
         if self.printer.connect():
-            print("Connected to the Printer.\n")
+            print("Connected to the Printer\n")
         else:
-            print("No Connection to Printerf\n")
+            print("No Connection to Printer\n")
 
     def start_edm(self):
         if self.edm.connect():
-            print("Connected to the EDM.\n")
+            print("Connected to the EDM\n")
         else:
             print("No Connection to EDM\n")
 
@@ -192,6 +205,16 @@ class MouseControlApp:
         self.distance = self.edm.capture_distance()
         self.dis_label.config(text=f"r: {round(self.distance,3)}")
 
+    def switch_laser(self):
+        self.laser_state = not self.laser_state
+        if self.laser_state:
+            state = "ON" 
+        else:
+            state = "OFF"
+        self.edm.laser(self.laser_state)
+        self.change_laser.config(text=f"Laser: {state}")
+        print(f"Laser turned {state}.\n")
+
     def switch_position(self):
         self.position = not self.position
         state = 1 if self.position else 2
@@ -218,6 +241,13 @@ class MouseControlApp:
         self.change_atr.config(text=f"ATR: {state}")
         print(f"ATR turned {state}.\n")
 
+    def calc_offset(self, distance):
+        if distance is not None:
+            offset_y = np.interp(distance, self.data_dist, self.data_y)
+            offset_x = np.interp(distance, self.data_dist, self.data_x)
+            return offset_x, offset_y
+        return None
+
     def draw(self):
         self.canvas.delete("all")
         if self.device is not None:
@@ -229,6 +259,50 @@ class MouseControlApp:
             frame = np.ndarray(buffer=array, dtype=np.uint8, shape=(item.height, item.width))
             self.corners, ids, _ = self.detector.detectMarkers(frame)
             frame_markers = aruco.drawDetectedMarkers(frame.copy(), self.corners, ids)
+            height, width = frame.shape
+
+            if self.atr_state and ids is not None:
+                for i, corner in enumerate(self.corners):
+                    # Calculate marker properties and ROI
+                    marker_id = ids[i][0]
+                    x_sum = self.corners[0][0][0][0] + self.corners[0][0][1][0] + self.corners[0][0][2][0] + self.corners[0][0][3][0]
+                    y_sum = self.corners[0][0][0][1] + self.corners[0][0][1][1] + self.corners[0][0][2][1] + self.corners[0][0][3][1]
+                    self.marker_coords = (x_sum * 0.25, y_sum * 0.25)
+
+                    offset_x, offset_y = -18, 120
+                    center_x, center_y = width // 2 + offset_x, height // 2 + offset_y
+                    
+                    roi_x, roi_y = 100, 140
+                    x1, y1 = max(center_x - roi_x, 0), max(center_y - roi_y, 0)
+                    x2, y2 = min(center_x + roi_x, width), min(center_y + roi_y, height)
+                    
+                    if x1 <= self.marker_coords[0] <= x2 and y1 <= self.marker_coords[1] <= y2:
+                        distance = self.edm.capture_distance()
+                        center_x_fine, center_y_fine = self.calc_offset(distance)
+                        #print(f"Center_y: {center_y_fine}")
+                        dist_x, dist_y = center_x_fine - self.marker_coords[0], center_y_fine - self.marker_coords[1]
+                        
+                    else:
+                        dist_x, dist_y = center_x - self.marker_coords[0], center_y - self.marker_coords[1]
+                
+                if self.atr_state:
+                    Kp_x = -0.005
+                else:
+                    Kp_x = 0.005
+                    
+                #print(f"Kp_x: {Kp_x}") 
+                Kp_y = 0.005
+
+                
+                #print(f"Dist: {dist_x,dist_y}")
+                if abs(dist_x) > 1  or abs(dist_y) > 1:
+                    self.x = self.x + Kp_x * dist_x
+                    self.y = self.y + Kp_y * dist_y
+                    move_x = f"{round((self.x), 2):06.2f}"
+                    move_y = f"{round((self.y), 2):06.2f}"
+                    move_command = f"G1 X{move_x} Y{move_y} F3600"
+                    self.printer.send_command(move_command)
+                
             # Capture frame-by-frame
             img = cv2.resize(frame_markers, (self.WIDTH, self.HEIGHT))
             img = Image.fromarray(img)
